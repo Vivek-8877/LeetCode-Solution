166f9564deba1d85cab4f48530677db3386b3fe0
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-09-13 21:34:14 +0530

    Create 393. UTF-8 Validation.java

---
+++ September 2022/Day 13/JAVA Code/393. UTF-8 Validation.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+        int strt = -1;
+        
+        for(int i=0;i<data.length;i++) {
+            
+            if(is_set(data[i],7)) {
+                if(is_set(data[i],6)) {
+                    if(is_set(data[i],5)) {
+                        if(is_set(data[i],4)) {
+                            if(is_set(data[i],3)) {
+                                // Invalid Case
+                                return false;
+                            } else {
+                                // 4 - Bytes
+                                if(strt!=-1) return false;
+                                strt=4;
+                            }
+                        } else {
+                            // 3 - Bytes
+                            if(strt!=-1) return false;
+                            strt=3;
+                        }
+                    } else {
+                        // 2 - Bytes
+                        if(strt!=-1) return false;
+                        strt=2;
+                    }
+                } else {
+                    // next - Bytes
+                    if(strt==-1) return false;
+                    strt--;
+                    if(strt==1) strt=-1;
+                }
+            } else {
+                // 1-Bytes
+                if(strt!=-1) return false;
+            }
+            
+        }
+        
+        if(strt!=-1) return false;
+        return true;
+    }
+    
+    public static boolean is_set(int n,int idx) {
+        return (n&(1<<idx))!=0;
+    }
+}
