be5da8d7abd2814cbdbd6a897f6247564d9b3454
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-10-24 11:26:38 +0530

    Create 1239. Maximum Length of a Concatenated String with Unique Characters.cpp

---
+++ October 2022/Day 24/C++ Code/1239. Maximum Length of a Concatenated String with Unique Characters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
+        vector<int> fre(26,0);
+        return f(arr,0,fre);
+    }
+    
+    int f(vector<string>& arr,int idx,vector<int>& fre) {
+        if(idx>=arr.size()) {
+            return 0;
+        }
+        int ans =0;
+        bool isValid = true;
+        
+        for(int i=0;i<arr[idx].length();i++) {
+            int ix = arr[idx][i]-'a';
+            if(fre[ix]>0) {
+                isValid=false;
+            }
+            fre[ix]++;
+        }
+        
+        if(isValid) {
+            int a1 = f(arr,idx+1,fre)+arr[idx].length();
+            ans = max(ans,a1);
+        }
+        
+        for(int i=0;i<arr[idx].length();i++) {
+            int ix = arr[idx][i]-'a';
+            fre[ix]--;
+        }
+        
+        int a2 = f(arr,idx+1,fre);
+        ans = max(ans,a2);
+        return ans;
+        
+    }
+    
+};
