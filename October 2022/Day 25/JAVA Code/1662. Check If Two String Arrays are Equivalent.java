38ed939dc0b50f84a29fa4cca97c6022ce9f3206
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-10-25 15:01:11 +0530

    Create 1662. Check If Two String Arrays are Equivalent.java

---
+++ October 2022/Day 25/JAVA Code/1662. Check If Two String Arrays are Equivalent.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for(String word : word1) sb1.append(word);
+        for(String word : word2) sb2.append(word);
+        return sb1.toString().compareTo(sb2.toString())==0;
+    }
+}
