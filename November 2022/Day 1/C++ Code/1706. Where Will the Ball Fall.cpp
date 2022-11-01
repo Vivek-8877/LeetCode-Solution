ed84f506a2c3a3c8e891c538ceaf300d3bdeef4f
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-01 19:07:37 +0530

    Create 1706. Where Will the Ball Fall.cpp

---
+++ November 2022/Day 1/C++ Code/1706. Where Will the Ball Fall.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid,int i,int j) {
+        if(i>=grid.size()) return j;
+        
+        if(i<0 || j<0 || j>=grid[i].size()) {
+            return -1;
+        }
+        
+        if(grid[i][j]==1) {
+            if(j+1<grid[i].size() && grid[i][j+1]==1) {
+                return dfs(grid,i+1,j+1);
+            } else {
+                return -1;
+            }
+        } else {
+            if(j-1>=0 && grid[i][j-1]==-1) {
+                return dfs(grid,i+1,j-1);
+            } else {
+                return -1;
+            }
+        }
+        
+    }
+    
+    vector<int> findBall(vector<vector<int>>& grid) {
+        vector<int> ans;
+        for(int i=0;i<grid[0].size();i++) {
+            ans.push_back(dfs(grid,0,i));
+        }
+        return ans;
+    }
+};
