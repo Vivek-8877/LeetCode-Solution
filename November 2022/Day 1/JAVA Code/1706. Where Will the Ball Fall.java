1e6ac7808d8a94c60683be2734125d63957b9b4b
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-01 19:08:14 +0530

    Create 1706. Where Will the Ball Fall.java

---
+++ November 2022/Day 1/JAVA Code/1706. Where Will the Ball Fall.java
@@ -0,0 +1,31 @@
+class Solution {
+    public static int dfs(int[][] grid,int i,int j) {
+        if(i>=grid.length) return j;
+        
+        if(i<0 || j<0 || j>=grid[i].length) {
+            return -1;
+        }
+        
+        if(grid[i][j]==1) {
+            if(j+1<grid[i].length && grid[i][j+1]==1) {
+                return dfs(grid,i+1,j+1);
+            } else {
+                return -1;
+            }
+        } else {
+            if(j-1>=0 && grid[i][j-1]==-1) {
+                return dfs(grid,i+1,j-1);
+            } else {
+                return -1;
+            }
+        }
+        
+    }
+    public int[] findBall(int[][] grid) {
+        int[] ans = new int[grid[0].length];
+        for(int i=0;i<ans.length;i++) {
+            ans[i]=dfs(grid,0,i);
+        }
+        return ans;
+    }
+}
