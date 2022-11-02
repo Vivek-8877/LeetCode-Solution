ea1f32153798d507379f738e31a755346f103066
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-02 19:08:05 +0530

    Create 433. Minimum Genetic Mutation.cpp

---
+++ November 2022/Day 2/C++ Code/433. Minimum Genetic Mutation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        vector<int> visit(bank.size(),0);
+        return f(start,end,bank,visit);
+    }
+    
+    int f(string start,string end,vector<string>& bank,vector<int>& visit) {
+        if(start==end) return 0;
+        
+        int ans=INT_MAX;
+        for(int i=0;i<bank.size();i++) {
+            if(visit[i]==0) {
+                int ct=0;
+                for(int j=0;j<start.length();j++) {
+                    if(start[j]!=bank[i][j]) {
+                        ct++;
+                    }
+                }
+                if(ct<2) {
+                    visit[i]=1;
+                    int val = f(bank[i],end,bank,visit);
+                    if(val!=-1) ans = min(ans,val+1);
+                    visit[i]=0;
+                }
+            }
+        }
+        if(ans==INT_MAX) ans=-1;
+        return ans;
+    }
+    
+};
