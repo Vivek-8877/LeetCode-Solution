b2f6f0017e4c20cf1e581de8f32e2e54d250562e
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2022-11-04 10:15:59 +0530

    Create 345. Reverse Vowels of a String.cpp

---
+++ November 2022/Day 4/C++ Code/345. Reverse Vowels of a String.cpp
@@ -0,0 +1,32 @@
+Problem Link :- https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+Video Solution Link :- 
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<char> vowels{'a','e','i','o','u','A','E','I','O','U'};
+        vector<char> list_of_vowels;
+        for(int i=0;i<s.length();i++) {
+            for(char vowel : vowels) {
+                if(vowel==s[i]) {
+                    list_of_vowels.push_back(vowel);
+                    break;
+                }
+            }
+        }
+        string ans="";
+        int idx = list_of_vowels.size()-1;
+        for(int i=0;i<s.length();i++) {
+            char c = s[i];
+            for(char vowel : vowels) {
+                if(vowel==s[i]) {
+                    c=list_of_vowels[idx];
+                    idx--;
+                    break;
+                }
+            }
+            ans+=c;
+        }
+        return ans;
+    }
+};
