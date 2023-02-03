112fadb3d48ef59c4239fc95112e2c14e362d1a7
VIVEK_8877 <91456523+Vivek-8877@users.noreply.github.com>
2023-02-03 06:47:52 +0530

    Added solution - LeetHub

--- Coordinates of the last cell in a Matrix on which performing given operations exits from the Matrix - GFG/coordinates-of-the-last-cell-in-a-matrix-on-which-performing-given-operations-exits-from-the-matrix.cpp
+++ Coordinates of the last cell in a Matrix on which performing given operations exits from the Matrix - GFG/coordinates-of-the-last-cell-in-a-matrix-on-which-performing-given-operations-exits-from-the-matrix.cpp
@@ -10,49 +10,50 @@ using namespace std;
 
 class Solution{
     public:
+    // 0 -> right , 1 -> down , 2 -> left , 3 -> up;
     pair<int,int> endPoints(vector<vector<int>> matrix){
         //code here
-            return f(matrix,0,0,0,-1,-1);
-    }
-    // 0 -> right , 1 -> down , 2 -> left , 3 -> up;
-    pair<int,int> f(vector<vector<int>>& a,int i,int j,int dir,int pi,int pj) {
-        if(i<0 || j<0 || i>=a.size() || j>=a[i].size()) {
-            pair<int,int> p1(pi,pj);
-            return p1;
-        }
+        int i=0,j=0,dir=0;
         pair<int,int> ans;
-        if(dir==0) {
-            if(a[i][j]==0) {
-                ans = f(a,i,j+1,dir,i,j);
+        while(i<matrix.size() && j<matrix[i].size()) {
+            ans.first=i;
+            ans.second=j;
+            if(dir==0) {
+                if(matrix[i][j]==0) {
+                    j++;
+                } else {
+                    matrix[i][j]=0;
+                    i++;
+                    dir=1;
+                }
+            } else if(dir==1) {
+                if(matrix[i][j]==0) {
+                    i++;
+                } else {
+                    matrix[i][j]=0;
+                    j--;
+                    dir=2;
+                }
+            } else if(dir==2) {
+                if(matrix[i][j]==0) {
+                    j--;
+                } else {
+                    matrix[i][j]=0;
+                    i--;
+                    dir=3;
+                }
             } else {
-                a[i][j]=0;
-                ans = f(a,i+1,j,1,i,j);
-            }
-        } else if(dir==1) {
-            if(a[i][j]==0) {
-                ans = f(a,i+1,j,dir,i,j);
-            } else {
-                a[i][j]=0;
-                ans = f(a,i,j-1,2,i,j);
-            }
-        } else if(dir==2) {
-            if(a[i][j]==0) {
-                ans = f(a,i,j-1,dir,i,j);
-            } else {
-                a[i][j]=0;
-                ans = f(a,i-1,j,3,i,j);
-            }
-        } else {
-            if(a[i][j]==0) {
-                ans = f(a,i-1,j,dir,i,j);
-            } else {
-                a[i][j]=0;
-                ans = f(a,i,j+1,0,i,j);
+                if(matrix[i][j]==0) {
+                    i--;
+                } else {
+                    matrix[i][j]=0;
+                    j++;
+                    dir=0;
+                }
             }
         }
         return ans;
     }
-
 };
 
 //{ Driver Code Starts.
